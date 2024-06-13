3ba84a16ef43a596f2bae60a43a0153a27b69174
sandeepkumar <sandeepkumar.bala@aspiresys.com>
2024-06-13 11:43:38 +0530

    Auth Config - Fixed

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
         clientId: "7eb7a16d-20ee-4642-97a0-83444cd5668c", // Your client ID here
         authority: "https://login.microsoftonline.com/42f65d85-0044-4e88-9d17-5aeff505072a", // Your tenant ID here
         redirectUri: "https://ashy-rock-0c7224700.5.azurestaticapps.net/",
-"
+
     },
     cache: {
         cacheLocation: "localStorage",
