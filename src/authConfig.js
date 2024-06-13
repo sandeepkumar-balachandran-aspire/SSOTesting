b5964686feb18b99e8661a256c828946e95ea4c6
sandeepkumar <sandeepkumar.bala@aspiresys.com>
2024-06-13 11:41:30 +0530

    Auth config change

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,9 +1,10 @@
 // authConfig.js
 export const msalConfig = {
     auth: {
-        clientId: "ff54779d-0063-4688-8f87-8b5d79ddb455", // Your client ID here
+        clientId: "7eb7a16d-20ee-4642-97a0-83444cd5668c", // Your client ID here
         authority: "https://login.microsoftonline.com/42f65d85-0044-4e88-9d17-5aeff505072a", // Your tenant ID here
-        redirectUri: window.location.origin
+        redirectUri: "https://ashy-rock-0c7224700.5.azurestaticapps.net/",
+"
     },
     cache: {
         cacheLocation: "localStorage",
